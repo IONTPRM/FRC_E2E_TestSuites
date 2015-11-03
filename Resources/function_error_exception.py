39b20d9d1ba8427d6023b99e4b741b06756f2846
Tilak Raj Singh <t.raj@iontrading.com>
2015-11-03 09:49:21 +0000

    Added new suites for sales trade matching

--- Resources/function_error_exception.py
+++
@@ -1,9 +0,0 @@
-class FunctionErrorException(Exception):
-	def __init__(self, function_name, error_code, error_msg):
-		super(FunctionErrorException, self).__init__(self._getExceptionMsg(function_name, error_code, error_msg))
-
-		self.error_code = error_code
-		self.error_msg = error_msg
-
-	def _getExceptionMsg(self, function_name, error_code, error_msg):
-		return 'Error executing function{%s} ret_code{%s} Msg{%s}' % (function_name, error_code, error_msg)
